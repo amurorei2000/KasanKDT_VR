ac5ae02312e406fba8b780a96baa8a9893987ce7
Wonseok Park <amurorei2000@gmail.com>
2024-02-14 16:17:31 +0900

    Initial commit

---
+++ Source/KDT_VR/Private/VRPlayer.cpp
@@ -0,0 +1,120 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VRPlayer.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Camera/CameraComponent.h"
+#include "MotionControllerComponent.h"
+#include "Components/TextRenderComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+
+AVRPlayer::AVRPlayer()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	cameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+	cameraComp->SetupAttachment(RootComponent);
+
+	headMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("HeadMesh"));
+	headMesh->SetupAttachment(cameraComp);
+
+	leftMotion = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("Left Motion Controller"));
+	leftMotion->SetupAttachment(RootComponent);
+	leftMotion->MotionSource = FName("Left");
+
+	leftHand = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Left Hand Mesh"));
+	leftHand->SetupAttachment(leftMotion);
+	leftHand->SetRelativeRotation(FRotator(0, 180, -270));
+
+	leftLog = CreateDefaultSubobject<UTextRenderComponent>(TEXT("Left Log"));
+	leftLog->SetupAttachment(leftMotion);
+	leftLog->SetRelativeLocation(FVector(20, 0, 0));
+	leftLog->SetRelativeRotation(FRotator(90, 180, 0));
+	leftLog->SetHorizontalAlignment(EHTA_Center);
+	leftLog->SetVerticalAlignment(EVRTA_TextCenter);
+	leftLog->SetWorldSize(20);
+	leftLog->SetTextRenderColor(FColor::Yellow);
+
+	rightMotion = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("Right Motion Controller"));
+	rightMotion->SetupAttachment(RootComponent);
+	rightMotion->MotionSource = FName("Right");
+
+	rightHand = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Right Hand Mesh"));
+	rightHand->SetupAttachment(rightMotion);
+	rightHand->SetRelativeRotation(FRotator(0, 0, 90));
+
+	rightLog = CreateDefaultSubobject<UTextRenderComponent>(TEXT("Right Log"));
+	rightLog->SetupAttachment(rightMotion);
+	rightLog->SetRelativeLocation(FVector(20, 0, 0));
+	rightLog->SetRelativeRotation(FRotator(90, 180, 0));
+	rightLog->SetHorizontalAlignment(EHTA_Center);
+	rightLog->SetVerticalAlignment(EVRTA_TextCenter);
+	rightLog->SetWorldSize(20);
+	rightLog->SetTextRenderColor(FColor::Yellow);
+}
+
+void AVRPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Input Mapping Context 파일을 입력 서브 시스템에 등록하는 절차를 실행한다.
+	APlayerController* pc = GetController<APlayerController>();
+	if (pc != nullptr)
+	{
+		UEnhancedInputLocalPlayerSubsystem* subsys = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(pc->GetLocalPlayer());
+
+		if (subsys != nullptr)
+		{
+			subsys->AddMappingContext(vrMapping, 0);
+		}
+
+	}
+
+}
+
+void AVRPlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AVRPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	UEnhancedInputComponent* enhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+
+	if (enhancedInputComponent != nullptr)
+	{
+		// 입력 컴포넌트에 RightTriggerTouch() 함수를 연결한다.(닿았을 때, 뗏을 때)
+		enhancedInputComponent->BindAction(rightTriggerTouch, ETriggerEvent::Started, this, &AVRPlayer::RightTriggerTouch);
+		enhancedInputComponent->BindAction(rightTriggerTouch, ETriggerEvent::Completed, this, &AVRPlayer::RightTriggerTouch);
+		enhancedInputComponent->BindAction(rightTriggerPress, ETriggerEvent::Started, this, &AVRPlayer::RightTriggerPress);
+		enhancedInputComponent->BindAction(rightTriggerPress, ETriggerEvent::Completed, this, &AVRPlayer::RightTriggerPress);
+		enhancedInputComponent->BindAction(rightTriggerValue, ETriggerEvent::Triggered, this, &AVRPlayer::RightTriggerValue);
+		enhancedInputComponent->BindAction(rightTriggerValue, ETriggerEvent::Completed, this, &AVRPlayer::RightTriggerValue);
+	}
+}
+
+void AVRPlayer::RightTriggerTouch(const FInputActionValue& val)
+{
+	// 입력받은 값을 오른손 로그에 출력한다.
+	FString result = val.Get<bool>() == true ? FString("True") : FString("False");
+	//rightLog->SetText(FText::FromString(FString::Printf(TEXT("Right Index touch: %s"), *result)));
+}
+
+void AVRPlayer::RightTriggerPress(const FInputActionValue& val)
+{
+	FString result = val.Get<bool>() == true ? FString("True") : FString("False");
+	//rightLog->SetText(FText::FromString(FString::Printf(TEXT("Right Index Pressed: %s"), *result)));
+}
+
+void AVRPlayer::RightTriggerValue(const FInputActionValue& val)
+{
+	float pressed = val.Get<float>();
+	rightLog->SetText(FText::FromString(FString::Printf(TEXT("Right Index Pressed: %.3f"), pressed)));
+}
+
---
+++ Source/KDT_VR/Public/VRPlayer.h
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "InputActionValue.h"
+#include "VRPlayer.generated.h"
+
+UCLASS()
+class KDT_VR_API AVRPlayer : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AVRPlayer();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(VisibleAnywhere, Category = "MySettings|Components")
+	class UCameraComponent* cameraComp;
+
+	UPROPERTY(VisibleAnywhere, Category = "MySettings|Components")
+	class UStaticMeshComponent* headMesh;
+
+	UPROPERTY(VisibleAnywhere, Category = "MySettings|Components")
+	class UMotionControllerComponent* leftMotion;
+
+	UPROPERTY(VisibleAnywhere, Category = "MySettings|Components")
+	class USkeletalMeshComponent* leftHand;
+
+	UPROPERTY(VisibleAnywhere, Category = "MySettings|Components")
+	class UTextRenderComponent* leftLog;
+
+	UPROPERTY(VisibleAnywhere, Category = "MySettings|Components")
+	class UMotionControllerComponent* rightMotion;
+
+	UPROPERTY(VisibleAnywhere, Category = "MySettings|Components")
+	class USkeletalMeshComponent* rightHand;
+
+	UPROPERTY(VisibleAnywhere, Category = "MySettings|Components")
+	class UTextRenderComponent* rightLog;
+
+	UPROPERTY(EditAnywhere, Category = "MySettings|Inputs")
+	class UInputMappingContext* vrMapping;
+
+	UPROPERTY(EditAnywhere, Category = "MySettings|Inputs")
+	class UInputAction* rightTriggerTouch;
+
+	UPROPERTY(EditAnywhere, Category = "MySettings|Inputs")
+	class UInputAction* rightTriggerPress;
+
+	UPROPERTY(EditAnywhere, Category = "MySettings|Inputs")
+	class UInputAction* rightTriggerValue;
+
+private:
+	void RightTriggerTouch(const FInputActionValue& val);
+	void RightTriggerPress(const FInputActionValue& val);
+	void RightTriggerValue(const FInputActionValue& val);
+};
